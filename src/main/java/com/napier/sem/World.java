cb1132739442ea060bcf6eafe1f0b7dbe980447f
josephfanning <jf338664@gmail.com>
2024-02-15 14:20:51 +0000

    made the communication between the database and dockers work

--- src/main/java/com/napier/sem/World.java
+++ src/main/java/com/napier/sem/World.java
@@ -2,5 +2,16 @@ package com.napier.sem;
 
 public class World {
 
+    // init ID
+    public int ID;
 
+    // init Name
+    public String Name;
+
+
+    public String CountryCode;
+
+    public String District;
+
+    public int Populaion;
 }
